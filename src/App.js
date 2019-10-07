31a04843102c2bb4e39729fd585a485c9fa3df8d
CarolEnache <enache.carol@gmail.com>
2019-10-07 10:32:12 +0100

    index and iterate the icons

--- src/App.js
+++ src/App.js
@@ -1,33 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import lizard from './Icons/lizard.png';
-import rock from './Icons/rock.png';
-import paper from './Icons/paper.png';
-import scissors from './Icons/scissors.png';
-import spock from './Icons/spock.png';
+
+import iconsList from './Icons/';
+
+console.log(iconsList)
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div></div>
-        <img src={lizard} className="App-logo" alt="logo" />
-        <img src={rock} className="App-logo" alt="logo" />
-        <img src={paper} className="App-logo" alt="logo" />
-        <img src={scissors} className="App-logo" alt="logo" />
-        <img src={spock} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {iconsList.map(icon => (
+          <button>
+            <img src={icon} alt='icon'/>
+          </button>
+        ))}
       </header>
     </div>
   );
---
+++ src/Icons/index.js
@@ -0,0 +1,9 @@
+import lizard from './lizard.png';
+import rock from './rock.png';
+import paper from './paper.png';
+import scissors from './scissors.png';
+import spock from './spock.png';
+
+const iconsList = [lizard, rock, paper, scissors, spock];
+
+export default iconsList;
